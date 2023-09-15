761851ad66494842f4760b440f87952c12f24ccf
Колесников Виталий <kolesnikovvitaliy@mail.ru>
2023-09-15 20:20:11 +0300

    8_4_8_recursion_dict_travel

--- Python_task/8_4_8_recursion_dict_travel.py
+++ Python_task/8_4_8_recursion_dict_travel.py
@@ -1,6 +1,5 @@
 def dict_travel(data: (object | dict), key=''):
     for k, v in sorted(data.items()):
-        print(k, v)
         if isinstance(v, dict):
             dict_travel(v, key + f'{k}.')
         else:
