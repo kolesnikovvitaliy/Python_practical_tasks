54604ede9f9d5e27f98b7cd5e7d436581438cbfb
Колесников Виталий <kolesnikovvitaliy@mail.ru>
2023-09-19 18:13:15 +0300

    9_8_22_decorator_isinstanse_args_func

--- Python_task/9_8_22_decorator_isinstanse_args_func.py
+++ Python_task/9_8_22_decorator_isinstanse_args_func.py
@@ -5,7 +5,7 @@ def takes(*datatype):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            result = map(lambda x: [False, True][isinstance(x, datatype)],
+            result = map(lambda x: isinstance(x, datatype),
                          [*args, *kwargs.values()])
             if all(result):
                 values = func(*args, **kwargs)
