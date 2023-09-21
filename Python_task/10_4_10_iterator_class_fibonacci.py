fb53632094aac8be8a21a1743ec9d527a40a4eb9
Колесников Виталий <kolesnikovvitaliy@mail.ru>
2023-09-21 17:28:43 +0300

    10_4_10_iterator_class_fibonacci

---
+++ Python_task/10_4_10_iterator_class_fibonacci.py
@@ -0,0 +1,10 @@
+class Fibonacci:
+    def __init__(self):
+        self.a, self.b = 0, 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.a, self.b = self.b, self.a + self.b
+        return self.a
