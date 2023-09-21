92f64c887cad032d7a65d6a9a755d8ed867dc99e
Колесников Виталий <kolesnikovvitaliy@mail.ru>
2023-09-21 11:50:07 +0300

    edited 10_2_22_get_min_max

--- Python_task/10_2_22_get_min_max.py
+++ Python_task/10_2_22_get_min_max.py
@@ -1,4 +1,24 @@
 def get_min_max(data):
-    if not data:
+    import copy
+    is_trye_range = copy.deepcopy(data)
+    if not hasattr(data, '__next__') and data:
+        return min(data), max(data)
+    elif hasattr(data, '__next__') and next(is_trye_range, 'stop') != 'stop':
+        min_range = copy.deepcopy(data)
+        max_range = copy.deepcopy(data)
+        return min(min_range), max(max_range)
+    else:
         return
-    return data.index(min(data)), data.index(max(data))
+
+# def get_min_max(iterable):
+#     iterable = iter(iterable)
+#     try:
+#         smallest = largest = next(iterable)
+#     except Exception:
+#         return None
+#     for elem in iterable:
+#         if elem < smallest:
+#             smallest = elem
+#         if elem > largest:
+#             largest = elem
+#     return smallest, largest
--- Python_task/_decorator_timer.py
+++ Python_task/_decorator_timer.py
@@ -2,10 +2,16 @@ import time
 
 
 def timer(func):
+    res = 0
+    
     def wrapper(*args, **kwargs):
+        nonlocal res
         start = time.perf_counter()
         val = func(*args, **kwargs)
         work_time = time.perf_counter() - start
-        print(f'Время выполнения {func.__name__}: {work_time} сек.')
+        res += work_time
+        # print(f'Время выполнения {func.__name__}: {work_time} сек.')
+        print(f'Время выполнения {func.__name__}: {res} сек.')
         return val
     return wrapper
+
