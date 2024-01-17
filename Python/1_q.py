62b16dc77ba8a34cbe142dc9fca1cee49013ee25
kolesnikovvitaliy <kolesnikovvitaliy@mail.ru>
2024-01-17 08:15:22 +0300

    redacted gitignore

--- Python/1_q.py
+++ Python/1_q.py
@@ -1,8 +1,12 @@
 import io, hashlib, hmac
-def my_func(a, b): 
-  ha = hashlib.sha1(a.encode('utf-8')).hexdigest() 
-  print(ha)
-  hb = hashlib.sha1(b.encode('utf-8')).hexdigest() 
-  print(hb)
-  return ha == hb
-print(my_func('a', 'a'))
+
+
+def my_func(a, b):
+    ha = hashlib.sha1(a.encode("utf-8")).hexdigest()
+    print(ha)
+    hb = hashlib.sha1(b.encode("utf-8")).hexdigest()
+    print(hb)
+    return ha == hb
+
+
+print(my_func("a", "a"))
--- Python_task/task.py
+++ Python_task/task.py
@@ -1,1 +1,24 @@
+from collections import ChainMap
 
+
+def get_value(chainmap, key, from_left=True):
+    if from_left:
+        if key in chainmap:
+            return chainmap[key]
+    else: 
+        if key in chainmap:
+            return [i[key] for i in reversed(chainmap.maps) if key in i][0]
+    return
+
+
+    # for dicts in chainmap.maps:
+    #     for k, v in dicts.items():
+    #         if key == k:
+    #             dicts[k] = value
+    # return
+
+
+
+chainmap = ChainMap({'name': 'Arthur'}, {'name': 'Timur'})
+
+print(get_value(chainmap, 'age'))
